d3dc0da36cde6c487bfdf7d04d1261fac6daf7fe
Zuber Ahmad <zahmad@rightsline.com>
2026-06-07 17:41:06 +0530

    added transation

---
+++ frontend/src/pages/transaction/Docket.jsx
@@ -0,0 +1,8 @@
+export default function DocketPage() {
+    return (
+        <div>
+            <h1>Docket Page</h1>
+            <p>This is the Docket page. Content will be added here.</p>
+        </div>
+    );
+}
---
+++ frontend/src/pages/transaction/TripSheet.jsx
@@ -0,0 +1,8 @@
+export default function TripSheetPage() {
+     return (
+        <div>
+            <h1>Trip Sheet Page</h1>
+            <p>This is the Trip Sheet page. Content will be added here.</p>
+        </div>
+    );
+}
