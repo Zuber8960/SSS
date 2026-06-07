9671d3d5c3e049db76417617d1106efc178f4de1
Zuber Ahmad <zahmad@rightsline.com>
2026-06-07 18:49:47 +0530

    removed port

--- backend/server.js
+++ backend/server.js
@@ -25,7 +25,7 @@ app.get('/', (req, res) => {
 const authRoutes = require('./routes/userRoutes');
 app.use('/api/auth', authRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
