b749003258e9053e32aeec187bfe9667502abf8e
Zuber Ahmad <zuberahmad8960@gmail.com>
2026-09-09 21:07:44 +0530

    Docket Serial Number fixes

--- backend/modules/docket/dcoket.routes.js
+++ backend/modules/docket/dcoket.routes.js
@@ -250,11 +250,12 @@ router.post('/', async (req, res) => {
     const firstDigit = String(Math.floor(Math.random() * 10));
     let docket_no = req.body.docket_no?.length > 0 ? req.body.docket_no : null;
     if (!docket_no) {
-      const [updatedRow] = await trx('sss.ssm_doc_control').where({ doc_type: 'DKT', loc_code })
-        .update({ last_upd_no: trx.raw('last_upd_no + 1') }).returning('last_upd_no');
+      const [updatedRow] = await trx('sss.ssm_doc_control').where({ doc_type: 'DKT', loc_code, company_code: tenant_id })
+        .update({ last_upd_no: trx.raw('last_upd_no + 1') }).returning('last_upd_no');  
 
-      const nextId = updatedRow.last_upd_no;
-      docket_no = String(moment().format('YY')) + firstDigit + locId + divisionId + nextId
+      // const nextId = updatedRow.last_upd_no;
+      // docket_no = String(moment().format('YY')) + firstDigit + locId + divisionId + nextId
+      docket_no = loc_code + 'CN' + String(updatedRow.last_upd_no).padStart(6, '0')
     }
     // const rest = require('crypto').randomBytes(7).toString('hex').toUpperCase().slice(0, 13);
     // const docket_no = firstDigit + rest;
